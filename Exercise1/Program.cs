24b18edbc3e42a8933636920b771df0fc30eb4c8
Aivenest <aivenest@gmail.com>
2022-09-18 21:07:57 +0500

    Exercise1 compleated

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1>number2)
+{
+    Console.WriteLine("Число " + number1 + " больше числа " + number2 + ".");
+}
+else if (number1<number2)
+{
+    Console.WriteLine("Число " + number2 + " больше числа " + number1 + ".");
+}
+else if (number1==number2)
+{
+    Console.WriteLine("Число " + number2 + " равно числу " + number1 + ".");
+}
