a024e63b9663451a4b16604293fa8540aa8a0bfc
Aivenest <aivenest@gmail.com>
2022-09-18 21:25:19 +0500

    Exercise2 compleated

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число.");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number3<number1&&number1>number2)
+{
+    Console.WriteLine("Максимальное число " + number1 + ".");
+}
+else if (number1<number2&&number2>number3)
+{
+    Console.WriteLine("Максимальное число " + number2 + ".");
+}
+else if (number1<number3&&number3>number2)
+{
+    Console.WriteLine("Максимальное число " +number3 + ".");
+}
+else if (number1==number2&&number1==number3&&number2==number3)
+{
+    Console.WriteLine("Числа равны.");
+}
+else if (number1==number2&&number2>number3&&number1>number3)
+{
+    Console.WriteLine("Числа " + number1 + " , " + number2 + " равны между собой и больше числа " + number3 + ".");
+}
+else if (number1==number3&&number3>number2&&number1>number2)
+{
+    Console.WriteLine("Числа " + number1 + " , " + number3 + " равны между собой и больше числа " + number2 + ".");
+}
+else if (number3==number2&&number2>number1&&number3>number1)
+{
+    Console.WriteLine("Числа " + number3 + " , " + number2 + " равны между собой и больше числа " + number1 + ".");
+}
+            
